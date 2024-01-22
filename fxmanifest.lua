e14d819807502e4ebdf648266018d6a119344214
Adriano Teixeira <63314477+AdrianoTxr@users.noreply.github.com>
2024-01-22 03:22:16 +0100

    fxmanifest add and improvements

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+
+author 'Adriano'
+description 'Ox Items to Qs Items Converter'
+version '1.0.0'
+
+game 'gta5'
+
+server_scripts {
+	"input/items.lua",
+	"server/main.lua"
+}
